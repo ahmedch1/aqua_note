074438a12bc70c6d1d42c9ca5109df5f1b446e41
Ahmed <ahmedchouihi@gmail.com>
2021-04-03 21:49:23 +0100

    'InsertingNEwObjects'

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -18,8 +18,23 @@ class Genus
     private $id;
 
     /**
-     * @ORM\Column(type="string")
-     */
+     * @ORM\Column(type="string")     */
     private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 
 }

d61e8fc7b1870452de63af661a2d16c1a298fca2
Ahmed <ahmedchouihi@gmail.com>
2021-04-03 08:43:50 +0100

    Initial commit joyful development with symfony

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,33 @@
+<?php
+// src/AppBundle/Controller/BlogController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends Controller
+{
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/blog", name="blog_list")
+     */
+    public function listAction($page=1)
+    {
+        // ...
+    }
+
+    /**
+     * Matches /blog/*
+     * but not /blog/slug/extra-part
+     *
+     * @Route("/blog/{slug}", name="blog_show")
+     */
+    public function showAction($slug)
+    {
+        // $slug will equal the dynamic part of the URL
+        // e.g. at /blog/yay-routing, then $slug='yay-routing'
+
+        // ...
+    }
+}
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+/**
+* @Route("/lucky/number/{max}")
+*/
+public function numberAction($max)
+{
+$number = random_int(0, $max);
+
+return new Response(
+'<html><body>Lucky number: '.$number.'</body></html>'
+);
+}
+}
